d25de214119190c7847858d925b7297f45b034a2
RCorpy <ramon.navm@gmail.com>
2023-02-23 19:14:35 +0100

    ProductSelector is done

--- src/renderer/assets/ProductSelector.tsx
+++ src/renderer/assets/ProductSelector.tsx
@@ -32,32 +32,86 @@ export default function ProductSelector({fullData}) {
     setCantidad(e.target.value)
   }
 
-  const calcularPrecio = () => {
-    return "Precio"
+  const calcularPrecio = (formula) => {
+    let thisFormula = fullData.formulas[formula]
+    let returnTotal = 0
+    Object.keys(thisFormula.components).forEach(component => {
+      if(fullData.rawMats[component]) returnTotal= returnTotal + (Number(thisFormula.components[component])*fullData.rawMats[component].price)
+      else {
+        console.log("else", calcularPrecio(component))
+        returnTotal = returnTotal + calcularPrecio(component) * Number(thisFormula.components[component])
+      }
+    })
+     return returnTotal
   }
 
   const makeTableRows = () => {
-    
+    let total = 0
     const localFunctionObject = product.components
 
     const returnValue = Object.keys(localFunctionObject).map(element=>{
+      
+      if(fullData.rawMats[element]){
+        //console.log("raw", element)
+        let precioLocal = fullData.rawMats[element].price 
+        
+        total = total + precioLocal * Number(localFunctionObject[element])
+
+        console.log(precioLocal, "->", total)
+        return (
+        <tr key={element}>
+              <th style={colSpans.n}>{fullData.rawMats[element].kkey}</th>
+              <th style={colSpans.nombre}>{element}</th>
+              <th style={colSpans.cantidad}>{Number(localFunctionObject[element]) * cantidad}</th>
+              <th style={colSpans.precio}>{precioLocal}</th>
+        </tr>
+      )
+    }
+    else if(fullData.formulas[element]){
+      //console.log("formula", element)
+      let precioLocal = calcularPrecio(element) 
+      
+      total = total + precioLocal * Number(localFunctionObject[element])
+
+      console.log(precioLocal, "->", total)
       return (
-      <tr key={element}>
-            <th style={colSpans.n}>{fullData.rawMats[element].kkey}</th>
-            <th style={colSpans.nombre}>{element}</th>
-            <th style={colSpans.cantidad}>{Number(localFunctionObject[element]) * cantidad}</th>
-            <th style={colSpans.precio}>{calcularPrecio()}</th>
-      </tr>
-    )})
+        <tr key={element}>
+              <th style={colSpans.n}>{fullData.formulas[element].kkey}</th>
+              <th style={colSpans.nombre}>{element}</th>
+              <th style={colSpans.cantidad}>{Number(localFunctionObject[element]) * cantidad}</th>
+              <th style={colSpans.precio}>{precioLocal}</th>
+        </tr>
+      )
+    }
+    else {
+      return (
+        <tr key={element}>
+              <th style={colSpans.n}>{"error"}</th>
+              <th style={colSpans.nombre}>{"material not found"}</th>
+              <th style={colSpans.cantidad}>{"error"}</th>
+              <th style={colSpans.precio}>{"error"}</th>
+        </tr>
+      )
+    }
     
-    return returnValue
+  
+  }
+  )
+    const filaTotales = (
+      <tr key={"totales"}>
+          <th style={colSpans.n}></th>
+          <th style={colSpans.nombre}>Precios</th>
+          <th style={colSpans.cantidad}>{total+"€/unidad"}</th>
+          <th style={colSpans.precio}>{"TOTAL "+total * cantidad +"€"}</th>
+      </tr>)
+    return <>{returnValue}{filaTotales}</>
   
 
   }
 
   return (
     <div>
-      <h1>{"product.name"}</h1>
+      <h1>{selectedProduct}</h1>
     <Table striped bordered hover responsive="md">
         <thead>
           <tr>
